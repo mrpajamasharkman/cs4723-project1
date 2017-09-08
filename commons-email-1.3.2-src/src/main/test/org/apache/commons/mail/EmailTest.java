123e74c4a4e46bfb662165af8950d0dafb9df08f
WoernerBro <seanwoerner.ru89@gmail.com>
2017-09-08 14:59:59 -0500

    Added @Test to all

--- commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
@@ -1,63 +1,78 @@
 package org.apache.commons.mail;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class EmailTest extends TestCase {
 
 	protected String email;
 	
-	public void testAddBcc() {
-		System.out.println("Running: testAddBcc");
-	}
-	
-	public void testAddCc() {
-		System.out.println("Running: testAddCc");
-	}
-	
-	public void testAddHeader() {
-		System.out.println("Running: testAddHeader");
-	}
-	
-	public void testAddReplyTo() {
-		System.out.println("Running: testAddReplyTo");
-	}
-	
-	public void testBuildMimeMessage() {
-		System.out.println("Running: testBuildMimeMessage");
-	}
-	
-	public void testGetHostName() {
-		System.out.println("Running: testGetHostName");
-	}
-	
-	public void testGetMailSession() {
-		System.out.println("Running: testGetMailSession");
-	}
-	
-	public void testGetSentDate() {
-		System.out.println("Running: testGetSentDate");
-	}
-	
-	public void testGetSocketConnectionTimeout() {
-		System.out.println("Running: testGetSocketConnectionTimeout");
-	}
-	
-	public void testSend() {
-		System.out.println("Running: testSend");
-	}
-	
-	public void testSetFrom() {
-		System.out.println("Running: testSetFrom");
-	}
-	
-	public void testUpdateContentType() {
-		System.out.println("Running: testContentType");
-	}
-	
 	@Override
 	protected void setUp() {
 		System.out.println("Running: setUp");
 		email = "lucastiedeman@gmail.com";
+	}
+	
+	@Test
+	public void testAddBcc() {
+		System.out.println("Running: testAddBcc");
+	}
+	
+	@Test
+	public void testAddCc() {
+		System.out.println("Running: testAddCc");
+		//assertEquals(addCc(email, null) == email);
+	}
+	
+	@Test
+	public void testAddHeader() {
+		System.out.println("Running: testAddHeader");
+	}
+	
+	@Test
+	public void testAddReplyTo() {
+		System.out.println("Running: testAddReplyTo");
+	}
+	
+	@Test
+	public void testBuildMimeMessage() {
+		System.out.println("Running: testBuildMimeMessage");
+	}
+	
+	@Test
+	public void testGetHostName() {
+		System.out.println("Running: testGetHostName");
+	}
+	
+	@Test
+	public void testGetMailSession() {
+		System.out.println("Running: testGetMailSession");
+	}
+	
+	@Test
+	public void testGetSentDate() {
+		System.out.println("Running: testGetSentDate");
+	}
+	
+	@Test
+	public void testGetSocketConnectionTimeout() {
+		System.out.println("Running: testGetSocketConnectionTimeout");
+	}
+	
+	@Test
+	public void testSend() {
+		System.out.println("Running: testSend");
+	}
+	
+	@Test
+	public void testSetFrom() {
+		System.out.println("Running: testSetFrom");
+	}
+	
+	@Test
+	public void testUpdateContentType() {
+		System.out.println("Running: testContentType");
 	}
 	
 	@Override
