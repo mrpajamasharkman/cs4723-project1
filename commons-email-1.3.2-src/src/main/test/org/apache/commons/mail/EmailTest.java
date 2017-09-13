ba0c8563bc3ab22fe9cacc46231425aa0d104e63
WoernerBro <seanwoerner.ru89@gmail.com>
2017-09-13 14:46:42 -0500

    Successfully created test cases for getSentDate(). Inserted base code for all test cases as comments.

--- commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
@@ -1,80 +1,227 @@
 package org.apache.commons.mail;
 
+import java.util.Date;
 import org.junit.Test;
 import junit.framework.TestCase;
+
 import org.easymock.*;
 
 public class EmailTest extends TestCase {
 
 	protected String email;
+	protected Email testEmail;
 	
 	@Override
 	protected void setUp() {
 		System.out.println("Running: setUp");
-		//EmailConstants testEmail = new EmailConstants();
+		testEmail = EasyMock.createMock(Email.class);
 		email = "lucastiedeman@gmail.com";
 	}
 	
 	@Test
 	public void testAddBcc() {
 		System.out.println("Running: testAddBcc");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
-	public void testAddCc() {
+	public void testAddCc() throws EmailException {
 		System.out.println("Running: testAddCc");
-		Email testEmail = EasyMock.createMock(Email.class);
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
 		
-	}
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
+		}
 	
 	@Test
 	public void testAddHeader() {
 		System.out.println("Running: testAddHeader");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testAddReplyTo() {
 		System.out.println("Running: testAddReplyTo");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testBuildMimeMessage() {
 		System.out.println("Running: testBuildMimeMessage");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testGetHostName() {
 		System.out.println("Running: testGetHostName");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testGetMailSession() {
 		System.out.println("Running: testGetMailSession");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testGetSentDate() {
 		System.out.println("Running: testGetSentDate");
+		
+		//record
+		EasyMock.expect(testEmail.getSentDate()).andReturn(testEmail.sentDate);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(testEmail.sentDate,testEmail.getSentDate());
+		EasyMock.verify(testEmail);
+	}
+	
+	@Test
+	public void testGetSentDateNull() {
+		System.out.println("Running: testGetSentDate");
+		Email testEmail = EasyMock.createMock(Email.class);
+		//record
+		EasyMock.expect(testEmail.getSentDate()).andReturn(new Date());
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(new Date(),testEmail.getSentDate());
+		EasyMock.verify(testEmail);
 	}
 	
 	@Test
 	public void testGetSocketConnectionTimeout() {
 		System.out.println("Running: testGetSocketConnectionTimeout");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testSend() {
 		System.out.println("Running: testSend");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testSetFrom() {
 		System.out.println("Running: testSetFrom");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Test
 	public void testUpdateContentType() {
 		System.out.println("Running: testContentType");
+		/*
+		//record
+		EasyMock.expect(testEmail.).andReturn(testEmail.);
+		
+		//replay
+		EasyMock.replay(testEmail);
+		
+		//verify
+		assertEquals(,testEmail.);
+		EasyMock.verify(testEmail);
+		*/
 	}
 	
 	@Override
@@ -83,6 +230,8 @@ public class EmailTest extends TestCase {
 		try{
 			email = null;
 			assertNull(email);
+			testEmail = null;
+			assertNull(testEmail);
 		}catch (NullPointerException npe){
 			System.out.println("Failed: tearDown");
 			System.out.println(npe);
