a1ec8f2138edeb33b80901fa3998e3c1ab460028
mrpajamasharkman <lucas.tiedeman@gmail.com>
2017-09-15 14:58:34 -0500

    5 tests done

--- commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/main/test/org/apache/commons/mail/EmailTest.java
@@ -6,38 +6,35 @@ import junit.framework.TestCase;
 
 public class EmailTest extends TestCase {
 
-	protected String address;
 	protected SimpleEmail email;
 	
 	@Override
 	protected void setUp() {
 		System.out.println("Running: setUp");
 		email = new SimpleEmail();
-		address = "lucastiedeman@gmail.com";
 	}
 	
 	@Test
-	public void testAddBcc() {
+	public void testAddBcc() throws EmailException {
 		System.out.println("Running: testAddBcc");
-		
+		assertEquals(email.addBcc("lucastiedeman@gmail.com"), email);
 	}
 	
 	@Test
 	public void testAddCc() throws EmailException {
 		System.out.println("Running: testAddCc");
-		assertEquals(email.addCc(address),email);
+		assertEquals(email.addCc("lucastiedeman@gmail.com"),email);
 	}
 	
 	@Test
 	public void testAddHeader() {
 		System.out.println("Running: testAddHeader");
-		
 	}
 	
 	@Test
-	public void testAddReplyTo() {
+	public void testAddReplyTo() throws EmailException {
 		System.out.println("Running: testAddReplyTo");
-		
+		assertEquals(email.addReplyTo("lucastiedeman@gmail.com"), email);
 	}
 	
 	@Test
@@ -61,7 +58,9 @@ public class EmailTest extends TestCase {
 	@Test
 	public void testGetSentDate() {
 		System.out.println("Running: testGetSentDate");
-		
+		assertEquals(email.getSentDate(), new Date());
+		email.setSentDate(new Date());
+		assertEquals(email.getSentDate(), new Date());
 	}
 	
 	@Test
@@ -77,9 +76,9 @@ public class EmailTest extends TestCase {
 	}
 	
 	@Test
-	public void testSetFrom() {
+	public void testSetFrom() throws EmailException {
 		System.out.println("Running: testSetFrom");
-		
+		assertEquals(email.setFrom("lucastiedeman@gmail.com"), email);
 	}
 	
 	@Test
@@ -94,11 +93,9 @@ public class EmailTest extends TestCase {
 		try{
 			email = null;
 			assertNull(email);
-			email = null;
-			assertNull(email);
 		}catch (NullPointerException npe){
-			System.out.println("Failed: tearDown");
-			System.out.println(npe);
+			System.err.println("Failed: tearDown");
+			System.err.println(npe);
 		}
 	}
 }
